ec20f32b0e838b1f926a26acb522ecc6f05b5ea7
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2021-04-02 14:01:35 +0200

    mongo connection

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ const devConfig = {
   //database
   jwt_key: "myS33!!creeeT",
   jwt_expiration: 360000,
-  dbConnectionString: `mongodb://localhost:27017/crud_api`,
+  dbConnectionString: `mongodb+srv://bap-express-api:2LaCaDCOlGaUNm2j@cluster0.znkuk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   mongoDebug: true
 };
 

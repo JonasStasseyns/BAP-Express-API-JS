1983f4a008ff2f4ef2322fbd21235d913feee408
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2021-04-02 14:04:17 +0200

    api merge

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ import { crudRoutes } from "../../module/crud/crud.routes";
 const apiRoutes = express.Router();
 
 apiRoutes.get("/", function(req, res, next) {
-  res.json({ message: "from index api" });
+  res.json({ message: "May the cors be with you." });
 });
 
 apiRoutes.use("/auth", crudRoutes);

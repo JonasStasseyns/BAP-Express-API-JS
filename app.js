b38960b605ca206b7932d290a0efb6f23ce797fc
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2021-04-05 15:05:06 +0200

    glitchio

--- app.js
+++ app.js
@@ -5,33 +5,29 @@ import apiRouter from "./api/routes";
 import connectMongo from "./config/mongoconnect";
 import cors from 'cors'
 import http from "http";
-import SocketIO from 'socket.io';
-
+import io from "socket.io"
 
 const app = express();
-app.use(cors())
 const server = http.createServer(app)
-let io = new SocketIO(server);
+let socketio = new io(server);
 
 // const corsOptions = {
 //   origin: 'https://bachelorproef-b2b80.web.app',
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-// Production environment
+// Production enviroment
 const isProduction = process.env.NODE_ENV === "production";
 app.use(bodyParser.json());
 
-io.on('connection', socket => {
-  console.log(socket)
-  socket.emit('test', 'hello')
-})
+
 
 //https debug
 app.use(morgan("dev"));
 
 //Connect Mongo
 connectMongo();
+app.use(cors())
 app.use("/api/v1", apiRouter);
 
 const PORT = process.env.PORT || 5000;
